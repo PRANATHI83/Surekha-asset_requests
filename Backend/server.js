918247921ce04ba340f66c53a711323ca80296e9
PRANATHI83 <apranu8@gmail.com>
2025-06-21 17:15:09 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3036;
+const port = 3009;
 
 // PostgreSQL connection configuration
 const pool = new Pool({
     user: 'postgres',
     host: 'postgres',
     database: 'asset_management',
-    password: 'admin1234',
+    password: 'admin234',
     port: 5432,
 });
 
@@ -169,5 +169,5 @@ app.delete('/api/requests/clear', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://3.108.193.252:${port}`);
+    console.log(`Server running at http://13.201.36.187:${port}`);
 });
